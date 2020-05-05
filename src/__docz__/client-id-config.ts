f5f52a23064326b1c4f3ee59fe8ecd281b080187
Dev Boldly <devboldly@gmail.com>
2020-05-05 16:45:02 -0500

    Update Client ID

--- src/__docz__/client-id-config.ts
+++ src/__docz__/client-id-config.ts
@@ -1,2 +1,2 @@
-const clientId = '932669268667-ptpd9g2buqb0qv56rk8ki8cpavedku83.apps.googleusercontent.com';
+const clientId = '843919432283-5qntd4ud7lid735vherta2vcgtp96u4r.apps.googleusercontent.com';
 export default clientId;
